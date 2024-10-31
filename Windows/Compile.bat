d1f4052f015e62b104982972c54f8595cf7dfa5c
Jacob Waters <jpwaters.github@gmail.com>
2024-10-31 18:07:01 +0000

    Update Compile.bat

--- Windows/Compile.bat
+++ Windows/Compile.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pyinstaller --noconsole --onefile --icon="Comment_Remover-v1.1.0-Windows-Icon.ico" --version-file="Comment_Remover-v1.1.0-Windows-Version_File" "Comment_Remover-v1.1.0-Windows.py"
+pyinstaller --noconsole --onefile --icon="Comment_Remover-v1.1.0-Windows-Icon.ico" "Comment_Remover-v1.1.0-Windows.py"
 
 del "Comment_Remover-v1.1.0-Windows.spec"
 rmdir /s /q build
