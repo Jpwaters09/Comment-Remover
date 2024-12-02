40125391cc417a5ef8953fcc6c49c3e4384a0651
Jacob Waters <jpwaters.github@gmail.com>
2024-12-02 20:25:52 +0000

    Delete Windows directory

--- Windows/Compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-pyinstaller --noconsole --onefile --icon="Comment_Remover-v1.1.0-Windows-Icon.ico" "Comment_Remover-v1.1.0-Windows.py"
-
-del "Comment_Remover-v1.1.0-Windows.spec"
-rmdir /s /q build
