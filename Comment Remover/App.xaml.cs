389ae9c24c8073476fb5f5f403d95c84c4f00aa6
Jacob Waters <jpwaters.github@gmail.com>
2024-12-02 20:36:26 +0000

    Create App.xaml.cs

---
+++ Comment Remover/App.xaml.cs
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////
+// Comment Remover - App.xaml.cs              //
+// Language: C#                               //
+// Author: Jacob Waters                       //
+// Github: github.com/jpwaters09              //
+// Copyright (c) 2024 Jacob Waters            //
+// Contact me: jpwaters.github@gmail.com      //
+////////////////////////////////////////////////
+
+using Microsoft.UI.Xaml;
+using Windows.Storage;
+
+namespace Comment_Remover
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            object themeSetting = ApplicationData.Current.LocalSettings.Values["themeSetting"];
+
+            if (themeSetting != null)
+            {
+                if ((int)themeSetting == 1)
+                {
+                    App.Current.RequestedTheme = (ApplicationTheme)0;
+                }
+
+                if ((int)themeSetting == 2)
+                {
+                    App.Current.RequestedTheme = (ApplicationTheme)1;
+                }
+            }
+
+            else
+            {
+                ApplicationData.Current.LocalSettings.Values["themeSetting"] = 0;
+            }
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+
+        private Window m_window;
+    }
+}
