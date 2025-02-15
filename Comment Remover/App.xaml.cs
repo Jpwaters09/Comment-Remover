dedb40d1db066982d543b4ae436112efcd3c54ca
Jacob Waters <jpwaters09.business@gmail.com>
2025-02-15 21:38:43 +0000

    Update App.xaml.cs

--- Comment Remover/App.xaml.cs
+++ Comment Remover/App.xaml.cs
@@ -7,29 +7,26 @@
 // Contact me: jpwaters.github@gmail.com      //
 ////////////////////////////////////////////////
 
-using Microsoft.UI.Xaml;
 using System;
+using Microsoft.UI.Xaml;
 
 namespace Comment_Remover
 {
     public partial class App : Application
     {
+        public static Window? m_window { get; private set; }
+
         public App()
         {
             this.InitializeComponent();
+
+            m_window = new MainWindow();
+            m_window.Activate();
         }
 
         public static TEnum GetEnum<TEnum>(string text) where TEnum : struct
         {
             return (TEnum)Enum.Parse(typeof(TEnum), text);
         }
-
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
-            m_window = new MainWindow();
-            m_window.Activate();
-        }
-
-        private Window m_window;
     }
 }
